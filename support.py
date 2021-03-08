b61150ea5caff75a93997ce22c33ccfce087bfc7
cylin1123 <l.chih.yu@gmail.com>
2021-03-09 04:54:33 +0800

    update 20210309

--- support.py
+++ support.py
@@ -1,11 +1,9 @@
-def print_func( name ):
-    print ("Hello,  ", name)
-    return
+def print_func(name):
+    return print ("Hello,  ", name)    
 
 def add_func(a,b):
     return a + b
 
 def sub_func(a,b):
     return a - b
-  
 
