d8a44c14ce7019deef816bfc6c8c3baca35b0645
cylin1123 <l.chih.yu@gmail.com>
2021-03-09 05:10:11 +0800

    update 20210309

--- support.py
+++ support.py
@@ -7,3 +7,9 @@ def add_func(a,b):
 def sub_func(a,b):
     return a - b
 
+# print('call it from support.py')
+# print_func("Nick")
+
+if __name__ == '__main__':
+    print('call it from support.py')
+    print_func("Nick")
